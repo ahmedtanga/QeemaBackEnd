ba764eea71c6bd9299a807fa9c05c816f023edce
Ahmed Tanga <aeltanga@ntgclarity.com>
2021-07-16 18:40:30 +0200

    update port

--- src/main/java/com/qeema/app/QeemaTask.java
+++ src/main/java/com/qeema/app/QeemaTask.java
@@ -2,9 +2,16 @@ package com.qeema.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class QeemaTask {
+public class QeemaTask extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(QeemaTask.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(QeemaTask.class, args);
